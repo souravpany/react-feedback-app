8cae0b1184d274e6183dadff45ac1538f5356cbe
sourav.kumar.pany <sourav.kumar.pany@gds.ey.com>
2022-11-10 19:39:09 +0530

    Feedback UI creation related code are added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import FeedbackList from "./components/FeedbackList";
+import Header from "./components/Header";
+import FeedbackData from "./data/FeedbackData";
 
 function App() {
+
+  const [feedback] = useState(FeedbackData);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <FeedbackList feedback={feedback} />
+    </>
   );
 }
 
